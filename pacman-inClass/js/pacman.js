a9bc39cb44a05e8ea831464b691e4767b208381b
idanstrul <98428527+idanstrul@users.noreply.github.com>
2022-01-25 23:33:17 +0200

    Add files via upload

---
+++ pacman-inClass/js/pacman.js
@@ -0,0 +1,93 @@
+'use strict'
+const PACMAN = '😷';
+
+var gPacman;
+
+function createPacman(board) {
+    gTotalFood--;
+    gPacman = {
+        location: {
+            i: 5,
+            j: 7
+        },
+        isSuper: false
+    }
+    board[gPacman.location.i][gPacman.location.j] = PACMAN
+}
+
+function movePacman(ev) {
+    if (!gGame.isOn) return
+    // use getNextLocation(), nextCell
+    var nextLocation = getNextLocation(ev)
+    // console.log('nextLocation', nextLocation)
+    var nextCell = gBoard[nextLocation.i][nextLocation.j]
+    // console.log('nextCell', nextCell)
+    // return if cannot move
+    if (nextCell === WALL) return
+    // hitting a ghost?  call gameOver
+    if (nextCell === GHOST) {
+        if (gPacman.isSuper) {
+            var deadGhost = killGhost(nextLocation);
+            if (deadGhost.currCellContent === FOOD){
+                updateScore(1);
+                deadGhost.currCellContent = EMPTY;
+            }
+        } else {
+            endGame();
+            return
+        }
+    }
+    if (nextCell === FOOD) {
+        updateScore(1);
+        if (gGame.score >= gTotalFood) endGame();
+    }
+    if (nextCell === POWERFOOD) {
+        if (gPacman.isSuper) return;
+        gPacman.isSuper = true;
+        renderAllGhosts();
+        setTimeout(function(){gPacman.isSuper = false}, 5000)
+        setTimeout(resurrectGhosts, 5000)
+    }
+
+    // moving from corrent position:
+    // update the model
+    gBoard[gPacman.location.i][gPacman.location.j] = EMPTY
+    // update the DOM
+    renderCell(gPacman.location, EMPTY)
+
+    // Move the pacman to new location
+    // update the model
+    gPacman.location = {
+        i: nextLocation.i,
+        j: nextLocation.j
+    }
+    gBoard[gPacman.location.i][gPacman.location.j] = PACMAN
+    // update the DOM
+    renderCell(gPacman.location, PACMAN)
+}
+
+function getNextLocation(keyboardEvent) {
+    // console.log('keyboardEvent.code', keyboardEvent.code)
+    // figure out nextLocation
+    var nextLocation = {
+        i: gPacman.location.i,
+        j: gPacman.location.j
+    }
+
+    switch (keyboardEvent.code) {
+        case 'ArrowUp':
+            nextLocation.i--
+            break;
+        case 'ArrowDown':
+            nextLocation.i++
+            break;
+        case 'ArrowLeft':
+            nextLocation.j--
+            break;
+        case 'ArrowRight':
+            nextLocation.j++
+            break;
+        default: return null
+    }
+    return nextLocation;
+}
---
+++ pacman-inClass/js/utils.js
@@ -0,0 +1,105 @@
+function createMat(ROWS, COLS) {
+  var mat = []
+  for (var i = 0; i < ROWS; i++) {
+    var row = []
+    for (var j = 0; j < COLS; j++) {
+      row.push('')
+    }
+    mat.push(row)
+  }
+  return mat
+}
+
+function printMat(mat, selector) {
+  var strHTML = '<table border="0"><tbody>';
+  for (var i = 0; i < mat.length; i++) {
+    strHTML += '<tr>';
+    for (var j = 0; j < mat[0].length; j++) {
+      var cell = mat[i][j];
+      var className = 'cell cell-' + i + '-' + j;
+      strHTML += '<td class="' + className + '">' + cell + '</td>'
+    }
+    strHTML += '</tr>'
+  }
+  strHTML += '</tbody></table>';
+  var elContainer = document.querySelector(selector);
+  elContainer.innerHTML = strHTML;
+}
+
+
+// goes throgh the entire data matrix, and checks isEmpty condition
+// for each cell. if true push to emptyCells than randomly sellect 
+// from empty cells. if there are no empty cells in data mat than  
+// return ifNoEmptyCells. 
+// returns a location object: { i, j }
+function getRndEmptyCellLocation(mat, isEmpty, ifNoEmptyCells) {
+  var emptyCells = [];
+  for (var i = 0; i < mat.length; i++) {
+    for (var j = 0; j < mat[0].length; j++) {
+      if (isEmpty) {
+        emptyCells.push({ i, j });
+      }
+    }
+  }
+
+  if (emptyCells.length > 0) {
+    var rndIdx = getRandomIntExclusive(0, emptyCells.length);
+    return emptyCells[rndIdx];
+  }
+
+  return ifNoEmptyCells;
+}
+
+
+
+// location such as: {i: 2, j: 7}
+function renderCell(location, value) {
+  // Select the elCell and set the value
+  var elCell = document.querySelector(`.cell-${location.i}-${location.j}`);
+  elCell.innerHTML = value;
+}
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+  var cellClass = 'cell-' + location.i + '-' + location.j;
+  return cellClass;
+}
+
+function getTime() {
+  return new Date().toString().split(' ')[4];
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+// Better chose ONLY ONE of the 2 following and use 
+// only it through the entire project:
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomIntExclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// creates modular field effect:
+function positiveModulo(m, n) { //Calculate positive m%n
+  return (m % n + n) % n;
+}
